cd9a4e06714daf3a38c196c8ad4653f52c540114
SibrenJacobs <sibren.jacobs@ucll.be>
2019-12-05 18:09:06 +0100

    qdf

--- Kassa/src/model/kortingStrategy/KortingStrategy.java
+++ Kassa/src/model/kortingStrategy/KortingStrategy.java
@@ -1,6 +1,13 @@
 package model.kortingStrategy;
 
+<<<<<<< HEAD
+import model.Product;
 import model.ShoppingCart;
+
+import java.util.List;
+=======
+import model.ShoppingCart;
+>>>>>>> cc64de83c4b89e3f5f5ebaec91b4f82bb287d643
 
 public interface KortingStrategy {
     double getKorting(ShoppingCart cart);
